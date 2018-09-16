33a9b97416cd0a2951a5f24b2a48f8c5785948c3
Genbu Hase <hasegenbu@gmail.com>
2018-09-16 18:05:00 +0900

    Google垢連携処理の実装

---
+++ libs/common.js
@@ -0,0 +1,34 @@
+const CLIENT_OPTIONS = {
+	apiKey: "AIzaSyB1VKWTrQ8d6yQ-5a7e_q_nz5xCpETrE60",
+	clientId: "894129288771-s8j9a9muf2cbs4vs79h1uhagcc73jrd2.apps.googleusercontent.com",
+	scope: "https://www.googleapis.com/auth/drive",
+	discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
+};
+
+const SIGNIN_OPTIONS = {
+	scope: "email openid",
+	redirect_uri: `${location.host === "seigakuin-pc-club.github.io" ? `${location.origin}/ColumnManager` : location.origin}/login`,
+
+	ux_mode: "popup"
+};
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+	if (gapi.client) return;
+
+	gapi.load("client:auth2", () => {
+		Promise.all([
+			gapi.client.init(CLIENT_OPTIONS).catch(error => { throw error }),
+			gapi.auth2.getAuthInstance()
+		]).then(() => {
+			return gapi.auth2.getAuthInstance().signIn(SIGNIN_OPTIONS);
+		}).then(() => {
+			console.log("Complated");
+		});
+	});
+});
+
+
+
+/* global gapi */
