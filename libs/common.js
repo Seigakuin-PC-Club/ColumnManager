8bb30173fc950ecff835dc8b382bd88a0853a763
Genbu Hase <hasegenbu@gmail.com>
2018-09-16 18:43:00 +0900

    ドロワー追加・ドメイン制限追加

--- libs/common.js
+++ libs/common.js
@@ -1,8 +1,21 @@
+// Materializeの初期化処理
+
+window.addEventListener("DOMContentLoaded", () => {
+	const sidenavs = document.querySelectorAll(".sidenav");
+	for (const sidenav of sidenavs) M.Sidenav.init(sidenav);
+});
+
+
+
+// Google APIとの連携処理
+
 const CLIENT_OPTIONS = {
 	apiKey: "AIzaSyB1VKWTrQ8d6yQ-5a7e_q_nz5xCpETrE60",
 	clientId: "894129288771-s8j9a9muf2cbs4vs79h1uhagcc73jrd2.apps.googleusercontent.com",
 	scope: "https://www.googleapis.com/auth/drive",
 	discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
+
+	hosted_domain: "seig-boys.jp"
 };
 
 const SIGNIN_OPTIONS = {
@@ -22,7 +35,7 @@ window.addEventListener("DOMContentLoaded", () => {
 			gapi.client.init(CLIENT_OPTIONS).catch(error => { throw error }),
 			gapi.auth2.getAuthInstance()
 		]).then(() => {
-			return gapi.auth2.getAuthInstance().signIn(SIGNIN_OPTIONS);
+			//return gapi.auth2.getAuthInstance().signIn(SIGNIN_OPTIONS);
 		}).then(() => {
 			console.log("Complated");
 		});
@@ -31,4 +44,5 @@ window.addEventListener("DOMContentLoaded", () => {
 
 
 
+/* global M */
 /* global gapi */
