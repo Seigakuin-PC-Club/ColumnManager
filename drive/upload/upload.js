6373eb4466a5e6a9cc649c9970c36d3f7c4b1e4c
Genbu Hase <hasegenbu@gmail.com>
2018-09-18 13:49:04 +0900

    直打ちフォーム追加

--- drive/upload/upload.js
+++ drive/upload/upload.js
@@ -1,10 +1,41 @@
 // ページ内要素定義
 
 const uploader = document.getElementById("uploader");
+const metadataForm = document.getElementById("uploader-metadata");
 const filenameInputter = document.getElementById("uploader-filename");
 const publishedAtPicker = document.getElementById("uploader-published-at");
+const mediaForm = document.getElementById("uploader-media");
 const columnTypePicker = document.getElementById("uploader-column-type");
-const columnPicker = document.getElementById("uploader-column");
+const columnFilePicker = document.getElementById("uploader-column--file");
+const columnTextInputter = document.getElementById("uploader-column--text");
+
+
+
+// メソッド定義
+
+class CMUploader {
+	static changeInputterStates (isFilePickerEnabled, isTextInputterEnabled) {
+		columnFilePicker.parentNode.classList[isFilePickerEnabled ? "remove" : "add"]("hide");
+		columnTextInputter.parentNode.classList[isTextInputterEnabled ? "remove" : "add"]("hide");
+
+		columnFilePicker.parentNode.querySelector(":scope .file-path").required = isFilePickerEnabled;
+		columnTextInputter.required = isTextInputterEnabled;
+		
+		if (!isFilePickerEnabled) columnFilePicker.accept = "";
+	}
+
+	static changeColumnType (columnType) {
+		switch (true) {
+			case !columnType:
+				return this.changeInputterStates(false, false);
+			default:
+				columnFilePicker.accept = columnType;
+				return this.changeInputterStates(true, false);
+			case columnType === "TEXT_DIRECT":
+				return this.changeInputterStates(false, true);
+		}
+	}
+}
 
 
 
@@ -12,10 +43,8 @@ const columnPicker = document.getElementById("uploader-column");
 
 window.addEventListener("DOMContentLoaded", () => {
 	uploader.addEventListener("submit", e => {
-		//e.preventDefault();
+		e.preventDefault();
 	});
 
-	columnTypePicker.addEventListener("change", e => {
-		columnPicker.accept = e.target.value;
-	});
+	columnTypePicker.addEventListener("change", e => CMUploader.changeColumnType(e.target.value));
 });
