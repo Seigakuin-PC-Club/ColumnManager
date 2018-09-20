ad4dfbd67b7c4e9ba40dfba41720e9fdcb4b788b
Genbu Hase <hasegenbu@gmail.com>
2018-09-20 21:08:36 +0900

    数種の変更 * drive/upload -> drive/uploader * アップロード時の不具合修正

--- drive/drive.js
+++ drive/drive.js
@@ -1,2 +1,2 @@
-// ?q=parents in "${DRIVE_DIR_ID}" and name contains "${user.uid}"
+// ?q=parents in "${DRIVE_DIR_ID}" and name contains "${user.uid}" and trashed != true
 // ?fields=files(id,name,originalFilename,properties/publishedAt,webContentLink,hasThumbnail,thumbnailLink)
